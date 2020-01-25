05e842fba0c4292430b49df91d34e7b34bc89e73
Moses <mosesejim@outlook.com>
2020-01-25 23:52:38 +0100

    fourth

--- resources/views/ssresponse.blade.php
+++ resources/views/ssresponse.blade.php
@@ -27,7 +27,7 @@
     
 
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
