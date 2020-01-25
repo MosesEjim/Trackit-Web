093cb6aa842cb32cd00fe9b6a5a8de05f75cadef
Moses <mosesejim@outlook.com>
2020-01-26 00:43:45 +0100

    fourth

--- resources/views/personnelstatistics.blade.php
+++ resources/views/personnelstatistics.blade.php
@@ -26,7 +26,7 @@
     
 
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
