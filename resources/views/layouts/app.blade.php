980576a8904e3c6bbe3f9349d7872c5b0848329e
mosesejim <mosesejim@outlook.com>
2020-02-19 20:24:34 +0100

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,13 +16,13 @@
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
     
-    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/cs-skin-elastic.css') }}">
-    <link rel="stylesheet" href="{{asset('vendors/jqvmap/dist/jqvmap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/bootstrap/dist/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/bootstrap/dist/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/bootstrap/dist/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/bootstrap/dist/css/cs-skin-elastic.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('vendors/jqvmap/dist/jqvmap.min.css') }}">
+    <link rel="stylesheet" href="{{secure_asset('assets/css/style.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
     @yield('script')
