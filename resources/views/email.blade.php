2ad84ff776b00ae85de527bfc9687dc81b935ffd
mosesejim <mosesejim@outlook.com>
2020-02-24 06:31:17 +0100

    update

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,4 +1,7 @@
 <h1>Hello {{$data['name']}}</h1>
+<p>
+Thank You For registering with Jmabea TrackIT-EUM
+</p>
 Your Login credentials are: <br>
 username: {{$data['name']}} <br>
-password: {{$data['password']}} <br>
+password: {{$data['email']}} <br>
